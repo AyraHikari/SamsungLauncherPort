d1e86058ee213c3bb2503694d56c7f1f7baec4b8
Ayra Hikari <tedyramadhani2@gmail.com>
2022-12-15 21:23:25 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,11 +1,5 @@
 #!/system/bin/sh
 
-# DT2S Fix
-[[ -e "/sys/touchpanel/double_tap" ]] && {
-  chmod 0666 "/sys/touchpanel/double_tap" 2>/dev/null
-  echo "1" > "/sys/touchpanel/double_tap"
-}
-
 # Increase system's smoothness and launcher's smoothness by increasing priority of some specified processes
 for pid in $(pidof -s surfaceflinger) $(pidof -s system_server) $(pgrep -f com.android.systemui) $(pgrep -f com.android.launcher3); do
   echo "-17" > "/proc/${pid}/oom_adj"
@@ -13,8 +7,8 @@ for pid in $(pidof -s surfaceflinger) $(pidof -s system_server) $(pgrep -f com.a
 done
 
 
+# Delaying for let the system boot
 sleep 15
-
 
 PKG=com.sec.android.app.launcher
 UID=$(pm list packages -U | grep $PKG | sed -n -e "s/package:$PKG uid://p")
@@ -25,7 +19,6 @@ pm grant $PKG android.permission.READ_EXTERNAL_STORAGE
 pm grant $PKG android.permission.WRITE_EXTERNAL_STORAGE
 pm grant $PKG android.permission.CALL_PHONE
 pm grant $PKG android.permission.READ_MEDIA_IMAGES
-
 
 appops set --uid $UID LEGACY_STORAGE allow
 appops set $PKG READ_CONTACTS allow
@@ -40,5 +33,5 @@ appops set $PKG GET_USAGE_STATS allow
 appops set $PKG REQUEST_DELETE_PACKAGES allow
 appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
 
-
+# Notification access fix
 cmd notification allow_listener com.sec.android.app.launcher/com.android.launcher3.notification.NotificationListener
